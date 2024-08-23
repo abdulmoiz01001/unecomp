d3ab8edeed040f21911dd27daf2cacd8d86425ac
abdulmoiz01001 <profession.ai01001@hotmail.com>
2024-08-24 00:46:18 +0500

    das

--- src/actions/emailResetAction.tsx
+++ src/actions/emailResetAction.tsx
@@ -2,31 +2,44 @@
 import { generateResetPasswordToken } from "@/lib/token";
 
 const emailResetAction = async (email : string) =>{
+  try{
+
     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user?user=${email}`, {
-        method: 'GET',
-      });
-  
-      if (!res.ok) {
-        
-        return { error: "Invalid email"};
-      }
-  
-      const resetPasswordToken =  await generateResetPasswordToken(email);
-
-      const send = await fetch(`${process.env.NEXTAUTH_URL}/api/resetpasswordtokenmail`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, resetPasswordToken }),
-      });
-
-        if (!send.ok) {
-            return { error: "Invalid email"};
-        }
-
-        return { success: "Email sent"};
-        
+      method: 'GET',
+    });
+    
+    if (!res.ok) {
+      
+      return { error: "Invalid email"};
+    }
+    
+    console.log('Email:', email);
+    console.log('User Response:', res);
+    
+    const resetPasswordToken =  await generateResetPasswordToken(email);
+    
+    console.log('Reset Password Token:', resetPasswordToken);
+    
+    const send = await fetch(`${process.env.NEXTAUTH_URL}/api/resetpasswordtokenmail`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, resetPasswordToken }),
+    });
+    
+    console.log('Send:', send);
+    
+    if (!send.ok) {
+      return { error: "Invalid email"};
+    }
+    
+    return { success: "Email sent"};
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    return { error: "An unexpected error occurred"};
+  }
+    
 }
 
 export default emailResetAction;
