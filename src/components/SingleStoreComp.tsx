8da898274cf64b1dcd3763956ac58e6742ea3c91
abdulmoiz01001 <profession.ai01001@hotmail.com>
2024-08-24 11:39:49 +0500

    doneee

--- src/components/SingleStoreComp.tsx
+++ src/components/SingleStoreComp.tsx
@@ -259,18 +259,18 @@ const SingleStoreComp = () => {
               </div>
               
               <div className="text-2xl font-semibold text-gray-800 mb-6">Total Price According to Quantity-{product.productPrice*productQuantity}</div>
-                <div className="flex xxs:flex-col xxs:justify-center items-end xs:gap-4 xs:justify-center items-end sm:gap-4 sm:justify-center items-end sm:gap-4 ">
+                <div className="flex xxs:flex-col xxs:justify-center xs:gap-4 xs:justify-center sm:gap-4 sm:justify-center items-end  ">
                   <span className="title-font font-medium text-2xl text-gray-900">RS-{product.productPrice}</span>
                   
                   <button onClick={() => { gotoDeliveryDetails(index) }} className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Order Now</button>
                   <Button variant='ghost' colorScheme='red' onClick={()=>{goBack()}} >
                   Discard
                 </Button>
-                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                     </svg>
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
